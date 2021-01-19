c07fea2180af70e7c4a4ecdab4902e8b68401538
pmarinelli18 <pmarinelli18@gmail.com>
2021-01-19 01:06:08 -0500

    made circles apear with react-native-game-engine

--- App.js
+++ App.js
@@ -1,21 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { PureComponent } from "react";
+import { StyleSheet, Text, View, AppRegistry, StatusBar } from "react-native";
+import { registerRootComponent } from "expo";
+import { Finger } from "./renderers";
+import { GameEngine } from "react-native-game-engine";
+// import Game from "./game";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class BestGameEver extends PureComponent {
+	constructor() {
+		super();
+	}
+
+	render() {
+		return (
+			<GameEngine
+				style={{ backgroundColor: "black" }}
+				entities={{
+					1: { position: [40, 200], renderer: <Finger /> },
+					2: { position: [100, 200], renderer: <Finger /> },
+					3: { position: [160, 200], renderer: <Finger /> },
+					4: { position: [220, 200], renderer: <Finger /> },
+					5: { position: [280, 200], renderer: <Finger /> },
+				}}
+			>
+				<Text style={[styles.setFontSize, styles.setColorRed]}>Hi</Text>
+				<StatusBar hidden={true} />
+			</GameEngine>
+		);
+	}
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "#FFF",
+	},
 });
+
+AppRegistry.registerComponent("BestGameEver", () => BestGameEver);
+
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>Open up App.js to start working on your app!</Text>
+//       <StatusBar style="auto" />
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+// 	container: {
+// 		flex: 1,
+// 		backgroundColor: "#fff",
+// 		alignItems: "center",
+// 		justifyContent: "center",
+// 	},
+// });
